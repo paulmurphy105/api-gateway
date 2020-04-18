e902c186e5c28736d52c6c50eb26a57f49ac80b1
Paul Murphy <paulmurphy105@gmail.com>
2020-04-18 09:25:29 -0700

    feat: refactor

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -22,20 +22,33 @@ module.exports = (redis) => {
       const tokensToAdd = Math.floor(elapsedSec / 15) // Add 1 token every 15 seconds since last request
       const newCacheLimit = cacheEntry.counter + tokensToAdd;
 
-      console.log(`elapsed: ${elapsedSec}`);
-      console.log(`tokensToAdd: ${tokensToAdd}`);
-      console.log(`newCacheLimit: ${newCacheLimit}`);
+      // console.log(`elapsed: ${elapsedSec}`);
+      // console.log(`tokensToAdd: ${tokensToAdd}`);
+      // console.log(`newCacheLimit: ${newCacheLimit}`);
+
+      let rateLimitRemaining
+      let rateLimitReset
 
       if (hitLimit(newCacheLimit)) {
         next(customError('You have hit your rate limit', 429))
       } else {
-        await setAsync(hash, JSON.stringify({ counter: newCacheLimit - 1, lastHit: Date.now() }), 'EX', 60)
+        await setAsync(hash, JSON.stringify({ counter: newCacheLimit - 1, lastHit: Date.now(), createdAt: cacheEntry.createdAt  }), 'EX', 60)
+        rateLimitRemaining = newCacheLimit - 1;
+        rateLimitReset = cacheEntry.createdAt + (60 * 1000)
       }
     }
 
     if (!cacheEntry) {
-      await setAsync(hash, JSON.stringify({ counter: 3, lastHit: Date.now() }), 'EX', 60)
+      await setAsync(hash, JSON.stringify({ counter: 3, lastHit: Date.now(), createdAt: Date.now() }), 'EX', 60)
+      rateLimitRemaining = 3
+      rateLimitReset = Date.now() + (60 * 1000)
     }
+
+    res.set({
+      'X-RateLimit-Limit': '3',
+      'X-RateLimit-Remaining': rateLimitRemaining,
+      'X-RateLimit-Reset': rateLimitReset
+    })
 
     next();
   }
